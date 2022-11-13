a71333f60f3343da8ce1d8b3a571920d95687b5f
sebas-777 <sebastiancast06@hotmail.com>
2022-11-13 02:31:51 -0500

    se aplican  estilos

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
-let searchButton = document.querySelector("#search")
+// let searchButton = document.querySelector("#search")
 
-searchButton.addEventListener("click", ()=> {
-    console.log("button clicked") 
-    sendApiRequest()
-}) 
+// searchButton.addEventListener("click", ()=> {
+//     console.log("button clicked") 
+//     sendApiRequest()
+// }) 
 
-async function sendApiRequest(){ 
+// async function sendApiRequest(){ 
+//     let API_KEY = "QIKdc7fAAoR7iEawSRI0cgGBYluASEaV4vqRW5sa"
+//     let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+//     console.log(response) 
+//     let data = await response.json()
+//     console.log(data)
+//     useApiData(data)
+// } 
+
+// function useApiData(data){ 
+// document.querySelector("#content").innerHTML += data.title
+// document.querySelector("#content").innerHTML += data.explanation
+// document.querySelector("#content").innerHTML += `<img src="${data.url}">`
+// }
+
+window.addEventListener('load',obtenerDatos);
+
+function obtenerDatos(){
     let API_KEY = "QIKdc7fAAoR7iEawSRI0cgGBYluASEaV4vqRW5sa"
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
-    console.log(response) 
-    let data = await response.json()
-    console.log(data)
-    useApiData(data)
+    let ruta = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}` 
+   
+
+    fetch(ruta)
+    .then(respuesta => respuesta.json())
+    .then(resultado => mostrarDatos(resultado))
 } 
 
-function useApiData(data){
-document.querySelector("#content").innerHTML += data.explanation
-document.querySelector("#content").innerHTML += `<img src="${data.url}">`
-}
+function mostrarDatos({date,explanation,media_type,title,url}) { 
+    const titulo = document.querySelector('#titulo')
+    titulo.innerHTML = title;
+    const fecha = document.querySelector('#fecha');
+    fecha.innerHTML = date;
+    const descripcion = document.querySelector('#descripcion');
+    descripcion.innerHTML = explanation;
+
+    const multimedia = document.querySelector('#c_multimedia');
+    if (media_type == 'video') { 
+        multimedia.innerHTML = `<iframe src="${url} "></iframe>`;
+        
+    } else{
+        multimedia.innerHTML = `<img src="${url}" alt="${url}">`
+    }
+
+} 
+
+
+
+
